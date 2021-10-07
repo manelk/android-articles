9fd8bb839c73360de380e6c26f015042b39cfc06
MohamedCharaabi <medch373@gmail.com>
2021-10-07 17:17:10 +0100

    add profile main screen

--- app/src/main/java/com/example/articlesproject/MainActivity.java
+++ app/src/main/java/com/example/articlesproject/MainActivity.java
@@ -1,48 +1,69 @@
 package com.example.articlesproject;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.Window;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.articlesproject.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getSupportActionBar().hide();
-//        getActionBar().hide();
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
+//Hide Action bar
+//        if (getSupportActionBar() != null) {
+//            getSupportActionBar().hide();
+//        }
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
-
+        DrawerLayout drawerLayout = binding.drawerLayout;
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_search,   R.id.navigation_bookmarks, R.id.navigation_profile)
+                .setOpenableLayout(drawerLayout)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-
+        NavigationUI.setupWithNavController(binding.drawerNavView, navController);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
 
 }
